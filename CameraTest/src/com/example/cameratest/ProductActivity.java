264bddea6ff68882858fbadf6c340a2302eecc3c
Lee <jessy1092@gmail.com>
2013-12-11 01:42:17 +0800

    Connect camera to take the product pictures.
    Signed-off-by: Lee <jessy1092@gmail.com>

--- CameraTest/src/com/example/cameratest/ProductActivity.java
+++ CameraTest/src/com/example/cameratest/ProductActivity.java
@@ -4,21 +4,41 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ProductActivity extends Activity
 {
+	TextView productView;
+	Button cameraButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.act_product);
+		findView();
+		setListener();
+		loadData();
 		
+	}
+	
+	private void findView()
+	{
+		productView = (TextView) findViewById(R.id.product_ID);
+		cameraButton = (Button) findViewById(R.id.button_camera);
+	}
+	
+	private void setListener()
+	{
+		cameraButton.setOnClickListener(btn_camera_click);
+	}
+	
+	private void loadData()
+	{
 		Intent intent = getIntent();
 		String productID = intent.getStringExtra("product_ID");
-		
-		TextView productView = (TextView) findViewById(R.id.product_ID);
 		productView.setText(productID);
 	}
 
@@ -29,5 +49,19 @@ public class ProductActivity extends Activity
 		getMenuInflater().inflate(R.menu.product, menu);
 		return true;
 	}
+	
+	private Button.OnClickListener btn_camera_click = new Button.OnClickListener()
+	{
+		@Override
+		public void onClick(View v)
+		{
+			// TODO Auto-generated method stub
+			Intent intent = new Intent(Intent.ACTION_VIEW);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+			intent.setClassName(ProductActivity.this, CameraTestActivity.class.getName());
+			ProductActivity.this.startActivity(intent);
+		}
+		
+	};
 
 }
